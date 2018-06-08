19c6db6bd3dce62df570ffe545d3b6d14f800a3a
Juri <juri.dispan@me.com>
2018-06-08 20:38:33 +0200

    Create getnudel.js

---
+++ getnudel.js
@@ -0,0 +1,17 @@
+function getRandomRedditPost(subreddit) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyStage == 4 && this.status == 200) {
+      var jsonObj = this.responseText;
+      var text = jsonObj[0].data.children.[0].data.selftext;
+      Console.log(text);
+      document.getElementById("displayText").innerHTML = text;
+    }
+  };
+  xhttp.open("GET", "http://www.reddit.com/r/" + subreddit + "/random/.json", true);
+  xhttp.send();
+}
+
+function randomKopiernudel() {
+  getRandomRedditPost("kopiernudeln");
+}
